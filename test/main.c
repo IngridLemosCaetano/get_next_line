48f003aa4544d1e49696ec6433edad75af06383f
Ingrid Lemos <ingridlc42@gmail.com>
2025-09-13 21:59:32 -0300

    update get_next_line

--- test/main.c
+++ test/main.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../functions/get_next_line.h"
 
 // int	main(void)
 // {
--- test/main_bonus.c
+++ test/main_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "../functions/get_next_line_bonus.h"
 
 int main(void)
 {
